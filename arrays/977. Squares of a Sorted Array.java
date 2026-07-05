394260d9756676b957db651b05fab7b30986425c
pradeepthi <pradeepthibokka@gmail.com>
2026-07-05 20:15:11 +0530

    updated

--- arrays/977. Squares of a Sorted Array.java
+++ arrays/977. Squares of a Sorted Array.java
@@ -21,3 +21,9 @@ class Solution {
      }    return result;                                      
 }
 }
+
+// in this problem we have i j for given array and k for result array k is an empty array where 
+// we will store thr values of the largest squares from the end in result array 
+// important thing is k is not pointer for nums array its only for resultant array for storing 
+// in this problem creating new array is imp
+// remaining is same like merge two sorted array problem 
