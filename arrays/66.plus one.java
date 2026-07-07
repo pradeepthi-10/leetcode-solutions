7fe2ae30dd00788b0f8890cc79933726db665862
pradeepthi <pradeepthibokka@gmail.com>
2026-07-07 20:13:03 +0530

    changes updated

--- arrays/66.plus one.java
+++ arrays/66.plus one.java
@@ -1,4 +1,4 @@
-public class Solution {
+ class Solution {
     public int[] plusOne(int[] digits) {
             for(int i=digits.length-1;i>=0;i--)
             {
@@ -19,6 +19,6 @@ public class Solution {
         
         }  
         
-    }   {
+    }  
     
-}
+
