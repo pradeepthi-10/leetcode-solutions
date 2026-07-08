148331b40d9d4c1d3d6cbca7c5b0c40ddf3606fb
pradeepthi <pradeepthibokka@gmail.com>
2026-07-08 19:55:35 +0530

    added solution

---
+++ sliding window/643. Maximum Average Subarray I.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        i
+        int right=k-1;
+        int sum = 0;
+        int max=0;
+        for(int i=0;i<k;i++)
+        {
+            sum+=nums[i];
+        }
+        max=sum;
+
+        for(right=k;right<nums.length;right++)
+        {
+          sum+=nums[right];
+          sum-=nums[right-k];
+          max=Math.max(sum,max);
+
+        }
+        return (double)max/k;
+    }
+
+    }
