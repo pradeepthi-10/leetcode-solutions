798eb4690e4f2f8bf9e21fd5cf8958eb6ce8451e
pradeepthi <pradeepthibokka@gmail.com>
2026-07-08 20:05:55 +0530

    solution updated

--- sliding window/643. Maximum Average Subarray I.java
+++ sliding window/643. Maximum Average Subarray I.java
@@ -21,3 +21,10 @@ class Solution {
     }
 
     }
+    // her in this prob we have to find the max average of sub array first if add is more avg will be more
+    // hence to find the max the size of subarray is fixed that is k first add the elements of k size subarray from beggining
+    // after addinng we then use a for loop in which right pointer is traversed untill the end
+    // now also remember to give sum= max because we use the formula math.max(sum,max) to find the max sum of subarray 
+    // in that formula max becomes intial sum and we also have to remove left pointer value from sum because we are moving the window to
+    // right and we have to add the right pointer value to sum because we are moving the window to right 
+    // at the end we will return the max sum/k because
