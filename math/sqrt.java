28949c91a9df036b42bf2f590fe461d70549038e
pradeepthi <pradeepthibokka@gmail.com>
2026-07-11 20:14:19 +0530

    updated

--- math/sqrt.java
+++ math/sqrt.java
@@ -29,4 +29,4 @@ public class sqrt {
 // used binary search to find the sqrt of a number because if we take the mid and multiply it with itself if it
 // is less than the number then we can move the low pointer to mid+1 because we have to find the max value of sqrt
 // so when we want sqrt of a number we can use binary search algorithm
-// first calculate a mid and it becomes easy the
+// first calculate a mid and it becomes easy to solve
